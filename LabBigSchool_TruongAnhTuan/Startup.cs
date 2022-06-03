cedd5442b0a39fa458194d829113389b535d3971
Anh Tuan <thttat2016@gmail.com>
2022-06-03 15:58:14 +0700

    Add project files.

---
+++ LabBigSchool_TruongAnhTuan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabBigSchool_TruongAnhTuan.Startup))]
+namespace LabBigSchool_TruongAnhTuan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ LabBigSchool_TruongAnhTuan/ViewModels/ValidTime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabBigSchool_TruongAnhTuan.ViewModels
+{
+    public class ValidTime : ValidationAttribute
+    {
+    }
+}
